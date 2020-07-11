b17002a678171862fca265c5829cd9718aeb7588
dongha <jamessleep@donghaui-MacBookPro.local>
2020-07-11 16:48:06 +0900

    Search Input

---
+++ screens/Search/SearchPresenter.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components/native";
+
+const Container = styled.ScrollView`
+  background-color: black;
+`;
+
+export default () => (
+  <Container>
+
+  </Container>
+);
