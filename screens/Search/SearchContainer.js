301c22b23f835d764c159708d0e6aa9e51d4714d
김동하 <dongha@gimdonghaui-Macmini.local>
2020-09-02 00:00:44 +0900

    #6.1 Searching Logic

--- screens/Search/SearchContainer.js
+++ screens/Search/SearchContainer.js
@@ -1,4 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import SearchPresenter from "./SearchPresenter";
+import { movieApi, tvApi } from "../../api";
 
-export default () => <SearchPresenter />;
+export default () => {
+  const [keyword, setKeyword] = useState("");
+  const [result, setResult] = useState({
+    movies: [],
+    shows: [],
+    movieError: null,
+    showsError: null
+  })
+  const onChange = (text) => setKeyword(text);
+  const search = async () => {
+    const [movies, movieError] = await movieApi.search(keyword);
+    const [shows, showsError] = await tvApi.search(keyword);
+    setResult({
+      movies,
+      shows,
+      movieError,
+      showsError,
+    })
+  }
+  return (
+    <SearchPresenter 
+      {...result}
+      onChange={onChange}
+      onSubmit={search}
+      keyword={keyword}
+    />
+  );
+}
